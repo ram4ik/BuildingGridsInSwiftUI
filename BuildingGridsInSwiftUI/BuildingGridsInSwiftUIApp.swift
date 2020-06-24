f0ac516d4e42256c0e83067524bfc56dcf14892e
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-24 20:46:40 +0300

    Initial Commit

---
+++ BuildingGridsInSwiftUI/BuildingGridsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingGridsInSwiftUIApp.swift
+//  BuildingGridsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct BuildingGridsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
